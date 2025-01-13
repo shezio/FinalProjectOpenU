e699c4c13d6c4a0288db3cc1f753567ceecb4cc6
sbunzel <shlomo.bunzel@cyberark.com>
2025-01-13 16:42:53 +0200

    login screen ready

--- childsmile/frontend/src/index.js
+++ childsmile/frontend/src/index.js
@@ -1,6 +1,6 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './components/Login';
+import Login from './Login';
+import './styles.css';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
--- childsmile/frontend/webpack.config.js
+++ childsmile/frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,18 +10,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 9000
   }
