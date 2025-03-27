a65ba260915ada64fe40db0e9e95e906af3e7552
shezio <shlezi0@gmail.com>
2025-03-27 18:33:01 +0200

    tasks flow part 2

--- childsmile/frontend/src/i18n.js
+++ childsmile/frontend/src/i18n.js
@@ -7,7 +7,17 @@ const resources = {
       "Login successful!": "Login successful!",
       "Invalid username": "Invalid username",
       "Invalid password": "Invalid password",
-      "An error occurred. Please try again.": "An error occurred. Please try again."
+      "An error occurred. Please try again.": "An error occurred. Please try again.",
+      "Technical Coordinator" : "Technical Coordinator",
+      "Volunteer Coordinator" : "Volunteer Coordinator",
+      "Families Coordinator" : "Families Coordinator",
+      "Tutors Coordinator" : "Tutors Coordinator",
+      "Matures Coordinator" : "Matures Coordinator",
+      "Healthy Kids Coordinator" : "Healthy Kids Coordinator",
+      "System Administrator" : "System Administrator",
+      "General Volunteer" : "General Volunteer",
+      "Tutor" : "Tutor",
+      "Select": "Select",
     }
   },
   he: {
@@ -15,7 +25,17 @@ const resources = {
       "Login successful!": "ההתחברות הצליחה!",
       "Invalid username": "שם משתמש שגוי או לא קיים אנא נסה שוב",
       "Invalid password": "סיסמה שגויה אנא נסה שוב",
-      "An error occurred. Please try again.": "אירעה שגיאה. אנא נסה שוב."
+      "An error occurred. Please try again.": "אירעה שגיאה. אנא נסה שוב.",
+      "Technical Coordinator" : "רכז טכני",
+      "Volunteer Coordinator" : "רכז מתנדבים",
+      "Families Coordinator" : "רכז משפחות",
+      "Tutors Coordinator" : "רכז חונכים",
+      "Matures Coordinator" : "רכז בוגרים",
+      "Healthy Kids Coordinator" : "רכז בריאים",
+      "System Administrator" : "מנהל מערכת",
+      "General Volunteer" : "מתנדב כללי",
+      "Tutor" : "חונך",
+      "Select": "בחר",      
     }
   }
 };
