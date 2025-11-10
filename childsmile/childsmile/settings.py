2bf556f1b6844f3290396039e90f3b2b9ca91a84
shezio <shlezi0@gmail.com>
2025-11-10 10:00:01 +0200

    allow bastion

--- childsmile/childsmile/settings.py
+++ childsmile/childsmile/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.31.51.180']
 
 
 # Application definition
