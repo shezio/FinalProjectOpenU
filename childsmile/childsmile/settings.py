36981901a67334f644b0b2400debca6a90d6d9d0
shezio <shlezi0@gmail.com>
2025-11-13 13:39:24 +0200

    alloowed hosts remove alb

--- childsmile/childsmile/settings.py
+++ childsmile/childsmile/settings.py
@@ -36,7 +36,7 @@ CLOUDFRONT_URL = "https://app.achildssmile.org.il"  # replace with your actual C
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.51.180','localhost','127.0.0.1','app.achildssmile.org.il','child-smile-app-alb-1403896092.il-central-1.elb.amazonaws.com']
+ALLOWED_HOSTS = ['172.31.51.180','localhost','127.0.0.1','app.achildssmile.org.il']#,'child-smile-app-alb-1403896092.il-central-1.elb.amazonaws.com']
 
 
 # Application definition
