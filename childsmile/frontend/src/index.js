82651514eb2e9bb5bbc042f9f7b53d27fd2f87a8
shezio <shlezi0@gmail.com>
2025-11-16 13:44:51 +0200

    import common

--- childsmile/frontend/src/index.js
+++ childsmile/frontend/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App'; // Import the App component
 import './i18n'; // Import i18n configuration
 import './styles.css';
+import './styles/common.css';  /* ✅ Import common.css directly */
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 //import { HashRouter as Router } from 'react-router-dom'; // Use HashRouter for better compatibility
 
