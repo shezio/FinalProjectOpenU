7c34afaa0035cf5e0443bc18ab696ca1e287055c
SHLOMO BUNZEL <34061265+shezio@users.noreply.github.com>
2025-12-23 10:57:13 +0200

    make deploy find childsmile_app
    Add BASE_DIR and update sys.path for WSGI configuration.

--- childsmile/wsgi.py
+++ childsmile/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)  # עכשיו כל import יחפש כאן קודם
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'childsmile.settings')
 
