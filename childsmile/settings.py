b1165c62e28c284a03e2745c1fbf0d6aea8c5988
shezio <shlezi0@gmail.com>
2025-12-24 15:21:13 +0200

    whitenoise install

--- childsmile/settings.py
+++ childsmile/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
